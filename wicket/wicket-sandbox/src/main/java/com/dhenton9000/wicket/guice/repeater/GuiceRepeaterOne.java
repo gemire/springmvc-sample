6e5f986b24e78a32712f035c491f3e677b2b2ae7
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2012-12-08 19:44:22 +0000

    demo of loadable detachable model and display

--- wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/guice/repeater/GuiceRepeaterOne.java
+++ wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/guice/repeater/GuiceRepeaterOne.java
@@ -4,8 +4,17 @@
  */
 package com.dhenton9000.wicket.guice.repeater;
 
+import com.dhenton9000.jpa.entities.Applications;
 import com.dhenton9000.wicket.TemplatePage;
+import com.dhenton9000.wicket.dao.IApplicationsDao;
+import com.google.inject.Inject;
+import java.util.List;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  *
@@ -13,11 +22,34 @@ import org.apache.wicket.markup.html.WebPage;
  */
 public final class GuiceRepeaterOne extends TemplatePage {
 
+    @Inject
+    private IApplicationsDao service;
     public GuiceRepeaterOne() {
-        
-        
-         this.setPageTitle("Simple Repeater Demo");
+
+
+        this.setPageTitle("Simple Repeater Demo");
+        addLocationsModule();
     }
-    
-  
+
+    private void addLocationsModule() {
+        ListView<Applications> applications = new ListView<Applications>("applications", createModelForLocations()) {
+            @Override
+            protected void populateItem(ListItem<Applications> item) {
+                item.add(new Label("id", new PropertyModel<Applications>(item.getModel(), "id")));
+                item.add(new Label("applicationName", new PropertyModel<Applications>(item.getModel(), "applicationName")));
+            }
+        };
+
+        add(applications);
+    }
+
+    private LoadableDetachableModel<List<Applications>> createModelForLocations() {
+
+        return new LoadableDetachableModel<List<Applications>>() {     
+            @Override
+            protected List<Applications> load() {     
+                return service.getAllApplications();
+            }
+        };
+    }
 }
