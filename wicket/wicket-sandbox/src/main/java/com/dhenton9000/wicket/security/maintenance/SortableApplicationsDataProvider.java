02301e4188f98ab5355cebc591425c2d5fa0df6f
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-03 15:11:51 +0000

    CRUD cycle seems to be working 2. Added true sorting. Hoping all this stuff works

--- wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/security/maintenance/SortableApplicationsDataProvider.java
+++ wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/security/maintenance/SortableApplicationsDataProvider.java
@@ -7,37 +7,61 @@ package com.dhenton9000.wicket.security.maintenance;
 import com.dhenton9000.jpa.entities.Applications;
 import com.dhenton9000.wicket.dao.IApplicationsDao;
 import com.google.inject.Inject;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * http://www.javabeat.net/2011/04/displaying-data-using-datatable-in-apache-wicket/
  *
  * @author dhenton
  */
 public class SortableApplicationsDataProvider extends SortableDataProvider<Applications, String> {
 
-    
+    private final Logger logger = LoggerFactory.getLogger(SortableApplicationsDataProvider.class);
     private IApplicationsDao applicationsService;
 
     /**
      * constructor
      */
-   
     SortableApplicationsDataProvider(IApplicationsDao applicationsService) {
-        
+
         setSort("applicationName", SortOrder.ASCENDING);
-        
+
         this.applicationsService = applicationsService;
     }
 
     @Override
     public Iterator<Applications> iterator(long first, long count) {
 
-        return applicationsService.getAllApplicationswithLimits(first,count).iterator();
+        //logger.debug("sort is " + getSort().getProperty() + " " + getSort().isAscending());
+        List<Applications> data = applicationsService.getAllApplications();
+        Collections.sort(data, new Comparator<Applications>() {
+            @Override
+            public int compare(Applications o1, Applications o2) {
+                int dir = getSort().isAscending() ? 1 : -1;
+                if ("applicationName".equals(getSort().getProperty())) {
+                    return dir * (o1.getApplicationName().
+                            toUpperCase().
+                            compareTo(o2.
+                            getApplicationName().toUpperCase()));
+                } else {
+                    return dir * (o1.getId().compareTo(o2.getId()));
+                }
+            }
+        });
 
-    }
+
+        return data.subList((int) first,
+                (int) Math.min(first + count, data.size())).iterator();
+
+   }
 
     /**
      * @see org.apache.wicket.markup.repeater.data.IDataProvider#size()
