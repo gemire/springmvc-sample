eefe4b302a90bbaa24b51b13f75ecd65b8f8f412
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-05 23:47:30 +0000

    FormInputWithList complete, but not submitting.

--- wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/dao/impl/RestaurantDaoImpl.java
+++ wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/dao/impl/RestaurantDaoImpl.java
@@ -4,9 +4,11 @@
  */
 package com.dhenton9000.wicket.dao.impl;
 
+import com.dhenton9000.jpa.dao.support.SearchTemplate;
 import com.dhenton9000.jpa.entities.Restaurant;
 import com.dhenton9000.wicket.dao.IRestaurantDao;
 import com.google.inject.Singleton;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,5 +25,17 @@ public class RestaurantDaoImpl
 
     public RestaurantDaoImpl() {
         super(Restaurant.class);
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+
+
+        SearchTemplate t = new SearchTemplate();
+        t.setNamedQuery("Restaurant.findAll");
+        // the applications object is a dummy to hold parameters 
+        // if the named query is parameterized.
+        return this.find(new Restaurant(), t);
+
     }
 }
