b2c083a2cc1cc21263dd9a41251b1e650794f98a
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-06 16:31:29 +0000

    modal window work

--- trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/modal/ModalInputPage.java
+++ trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/modal/ModalInputPage.java
@@ -13,18 +13,23 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- *
+ * This is a demonstration of passing information to a modal dialog
+ * and getting it back.
+ * 
  * @author dhenton
  */
 public final class ModalInputPage extends TemplatePage {
 
-    private Object text;
-    private Object itemselection;
+    private final Logger logger = LoggerFactory.getLogger(ModalInputPage.class);
     private ChooserPanel chooserPanel;
-    private TextField textField;
+    private Form<ModalData> form = null;
+    private ModalData formData = new ModalData();
 
     public ModalInputPage() {
         super();
@@ -36,55 +41,119 @@ public final class ModalInputPage extends TemplatePage {
     }
 
     public void setup() {
-        setPageTitle(getClass().getCanonicalName());
-        Form form = new Form("form", new CompoundPropertyModel(this)) {
+        setPageTitle(getClass().getSimpleName());
+        form = new Form<ModalData>("form", new CompoundPropertyModel(
+                new PropertyModel(ModalInputPage.this, "formData"))) {
             @Override
             protected void onSubmit() {
-                System.out.println("form submitted.");
+                logger.debug("form submitted.");
             }
         };
+        form.setOutputMarkupId(true);
         add(form);
-        form.add(new Label("label", "Item codes"));
-        textField = new TextField("text");
-        textField.setOutputMarkupId(true);
-        form.add(textField);
+        form.add(new Label("ageLabel", "Age"));
+        form.add(new Label("nameLabel", "Name"));
+        TextField ageField = new TextField("age");
+        form.add(ageField);
+        TextField nameField = new TextField("name");
+        form.add(nameField);
         form.add(new Button("button"));
 
         final ModalWindow chooserWindow = new ModalWindow("chooserWindow");
         chooserWindow.setOutputMarkupId(true);
-        form.add(chooserWindow);
-        chooserPanel = new ChooserPanel(chooserWindow.getContentId(), chooserWindow);
+        chooserWindow.setInitialWidth(200);
+        chooserWindow.setInitialWidth(350);
+        add(chooserWindow);
+        // the CompounPropertyModel will allow the use of reflection
+        // to fill out the form elements in the html based off the object
+        // the property model means that the property will be evaluated each
+        // time in the cycle, thus reflecting updates
+        
+        chooserPanel = new ChooserPanel(chooserWindow.getContentId(),
+                new CompoundPropertyModel(
+                new PropertyModel(ModalInputPage.this, "formData")));
 
         chooserWindow.setContent(chooserPanel);
-        chooserWindow.setTitle("Make your choice");
+        chooserWindow.setTitle("Enter Your Data");
+        // to remember window positioning
         chooserWindow.setCookieName("chooserWindow-1");
 
         chooserWindow.setCloseButtonCallback(new ModalWindow.CloseButtonCallback() {
             @Override
             public boolean onCloseButtonClicked(AjaxRequestTarget target) {
+                // this does nothing, and since the form wasn't modified
+                // in the dialog, nothing will change on this form
                 return true;
             }
-
-             
         });
 
         chooserWindow.setWindowClosedCallback(new ModalWindow.WindowClosedCallback() {
             @Override
             public void onClose(AjaxRequestTarget target) {
-                  textField.setModelObject(chooserPanel.getSelectedModel() );
-                 target.add(textField);
-                
-                
+
+                target.add(form);
+
+
             }
         });
 
         form.add(new AjaxLink("chooserLink") {
-            
             @Override
             public void onClick(AjaxRequestTarget target) {
                 chooserWindow.show(target);
             }
         });
 
+    }
+
+    /**
+     * @return the formData
+     */
+    public ModalData getFormData() {
+        return formData;
+    }
+
+    /**
+     * @param formData the formData to set
+     */
+    public void setFormData(ModalData formData) {
+        this.formData = formData;
+    }
+
+    /**
+     * the form 'backing bean' if you will
+     */
+    public class ModalData {
+
+        private String name = "[Type Name]";
+        private Integer age = 25;
+
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @param name the name to set
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @return the age
+         */
+        public Integer getAge() {
+            return age;
+        }
+
+        /**
+         * @param age the age to set
+         */
+        public void setAge(Integer age) {
+            this.age = age;
+        }
     }
 }
