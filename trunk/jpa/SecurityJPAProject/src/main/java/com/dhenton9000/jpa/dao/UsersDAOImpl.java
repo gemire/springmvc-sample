2dbcb220e40cc3b5405d59869ac10f836104894f
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-08 01:58:49 +0000

    updated for of rename GenericImpl

--- trunk/jpa/SecurityJPAProject/src/main/java/com/dhenton9000/jpa/dao/UsersDAOImpl.java
+++ trunk/jpa/SecurityJPAProject/src/main/java/com/dhenton9000/jpa/dao/UsersDAOImpl.java
@@ -5,7 +5,7 @@
 package com.dhenton9000.jpa.dao;
 
 import com.dhenton9000.jpa.entities.Users;
-import com.dhenton9000.jpa.dao.hibernate.HibernateGenericDao;
+import com.dhenton9000.jpa.dao.hibernate.HibernateGenericDaoImpl;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
  * @author dhenton
  */
 @Repository
-public class UsersDAOImpl extends HibernateGenericDao<Users, String> implements UsersDAO {
+public class UsersDAOImpl extends HibernateGenericDaoImpl<Users, String> implements UsersDAO {
     
     
     public UsersDAOImpl() {
