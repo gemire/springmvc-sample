95fca304fbecd6d079c4cb1e1e3ac7d026d8aa0d
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-15 00:53:33 +0000

    initial import

---
+++ trunk/xml/StaxProject/src/main/java/com/dhenton9000/staxproject/App.java
@@ -0,0 +1,17 @@
+package com.dhenton9000.staxproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        
+    }
+}
