a3e9a52ea5ace723003f492755d44562b95010f5
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-01-25 21:14:02 +0000

    completed error  response handling

--- trunk/mule/mule32/MuleSyncWebService/src/main/java/com/dhenton9000/registration/csv/CsvMuleComponent.java
+++ trunk/mule/mule32/MuleSyncWebService/src/main/java/com/dhenton9000/registration/csv/CsvMuleComponent.java
@@ -14,35 +14,34 @@ import org.mule.api.transport.PropertyScope;
 import org.apache.log4j.*;
 import org.mule.api.MuleMessage;
 import com.dhenton9000.registration.bindings.RegisterResponse;
+
 /**
- * This mule component delegates to the WebServiceFileInputReader
- * which dispatches for each line.
+ * This mule component delegates to the WebServiceFileInputReader which
+ * dispatches for each line.
+ *
  * @author dhenton
  */
-public class CsvMuleComponent implements Callable  {
+public class CsvMuleComponent implements Callable {
 
-    
     private String dispatchDestination;
-    
+    private String errorDestination;
     private WebServiceFileInputReader fileReader;
     private static Logger log = LogManager.getLogger(CsvMuleComponent.class);
+    private String processingType = null;
 
     //this is the mule message property key
     @Override
     public Object onCall(MuleEventContext eventContext) throws Exception {
 
         InputStream is = (InputStream) eventContext.getMessage().getPayload();
-	MuleWebServiceTransmitter webSender = new MuleWebServiceTransmitter();
-	webSender.setMuleEventContext(eventContext);
-	getFileReader().setWebServiceTransmitter(webSender);
-	getFileReader().processInput(is);
-	is.close();
+        MuleWebServiceTransmitter webSender = new MuleWebServiceTransmitter();
+        webSender.setMuleEventContext(eventContext);
+        getFileReader().setWebServiceTransmitter(webSender);
+        getFileReader().processInput(is);
+        is.close();
         return null;
     }
 
-   
-    
- 
     /**
      * @return the dispatchDestination
      */
@@ -70,31 +69,81 @@ public class CsvMuleComponent implements Callable  {
     public void setFileReader(WebServiceFileInputReader fileReader) {
         this.fileReader = fileReader;
     }
-    ///////////////////////
+
     /**
-     * this class fills the contract of the web transmitter via mule
-     * it derives all its properties from mule via Spring and being an
-     * inner class
+     * @return the processingType
+     */
+    public String getProcessingType() {
+        return processingType;
+    }
+
+    /**
+     * @param processingType the processingType to set
+     */
+    public void setProcessingType(String processingType) {
+        this.processingType = processingType;
+    }
+
+    /**
+     * @return the errorDestination
+     */
+    public String getErrorDestination() {
+        return errorDestination;
+    }
+
+    /**
+     * @param errorDestination the errorDestination to set
+     */
+    public void setErrorDestination(String errorDestination) {
+        this.errorDestination = errorDestination;
+    }
+    ///////////////////////
+
+    /**
+     * this class fills the contract of the web transmitter via mule it derives
+     * all its properties from mule via Spring and being an inner class has
+     * access to the mulecontext
      */
     class MuleWebServiceTransmitter implements WebServiceTransmitter {
 
         private MuleEventContext muleEventContext = null;
 
         @Override
-        public void transmit(RegisterInput request) {
+        public TransmitResult transmit(RegisterInput request, long currentLine, String[] lineContents) {
+
+            TransmitResult tResult = new TransmitResult();
+            tResult.setLineNumber(currentLine);
+            tResult.setSuccessful(true);
+            tResult.setLineContents(TransmitResult.convertArrayToCSVLine(lineContents));
+            String fileName = muleEventContext.getMessage()
+                    .getProperty("originalFileName", PropertyScope.OUTBOUND);
+            tResult.setFileName(fileName);
+            tResult.setMessage(null);
 
             DefaultMuleMessage dM = new DefaultMuleMessage(request, muleEventContext.getMuleContext());
             MuleMessage reply = null;
+  
             try {
-               reply = muleEventContext.sendEvent(dM, dispatchDestination);
-               //log.info("reply in transmitter is "+reply.getPayload().getClass().getName());
-               RegisterResponse response = (RegisterResponse) reply.getPayload();
-               log.info("reply is "+response.getResponseInformation());
-                       
-               //        muleEventContext.dispatchEvent(dM, dispatchDestination);
+                // sync dispatch to the web service
+                reply = muleEventContext.sendEvent(dM, dispatchDestination);
+                // answer back
+                RegisterResponse response = (RegisterResponse) reply.getPayload();
+                log.debug("reply is " + response.getResponseInformation());
+                // check status
+                if (!response.getResponseStatus().equals("OK")) {
+                    tResult.setSuccessful(false);
+                    tResult.setMessage(response.getResponseInformation());
+                    dM = 
+                    new DefaultMuleMessage(tResult, muleEventContext.getMuleContext());
+                    //async dispatch to the line error handler
+                    muleEventContext.dispatchEvent(dM, getErrorDestination());
+                }
+
             } catch (MuleException e) {
                 throw new RuntimeException("Mule dispatch problem: " + e.getMessage());
             }
+
+            return tResult;
         }
 
         public void setMuleEventContext(MuleEventContext muleEventContext) {
--- trunk/mule/mule32/MuleSyncWebService/src/main/java/com/dhenton9000/registration/csv/WebServiceTransmitter.java
+++ trunk/mule/mule32/MuleSyncWebService/src/main/java/com/dhenton9000/registration/csv/WebServiceTransmitter.java
@@ -11,6 +11,15 @@ import com.dhenton9000.registration.bindings.RegisterInput;
  * @author dhenton
  */
 public interface WebServiceTransmitter {
-
-	void transmit(RegisterInput request);
+        
+        /**
+         * send a RegisterInput request to a web service and return a 
+         * TransmitResult which contains information about the communication
+         * @param request the request to send
+         * @param currentLine the linenumber that this transmission is derived from
+         * @param the contents of the line as a String array
+         * 
+         * @return information about the success of the transmission
+         */
+	TransmitResult transmit(RegisterInput request, long currentLine,String[] lineContents);
 }
