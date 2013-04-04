05e36b4caf358dd8b25d1190ecc95de922b847dc
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-04-04 20:00:27 +0000

    added port client that can be used in a functional test to talk to a mule web service

--- mule/mule32/MuleWSSandbox/src/test/java/com/dhenton9000/ws/sandbox/MuleLauncher.java
+++ mule/mule32/MuleWSSandbox/src/test/java/com/dhenton9000/ws/sandbox/MuleLauncher.java
@@ -7,8 +7,12 @@ package com.dhenton9000.ws.sandbox;
 import com.dhenton9000.registration.bindings.RegisterInput;
 import com.dhenton9000.registration.bindings.RegisterResponse;
 import com.dhenton9000.registration.bindings.RegistrationDetails;
+import com.dhenton9000.registration.components.ws.RegistrationServiceWSDL;
+import com.dhenton9000.registration.components.ws.RegistrationServiceWSDLClient;
 import java.math.BigInteger;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
 import org.mule.api.MuleContext;
 import org.mule.config.spring.SpringXmlConfigurationBuilder;
 import org.mule.context.DefaultMuleContextFactory;
@@ -16,6 +20,7 @@ import org.apache.log4j.*;
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.LocalMuleClient;
+import org.mule.api.transformer.Transformer;
 import org.mule.api.transformer.TransformerException;
 import org.mule.module.xml.transformer.jaxb.JAXBUnmarshallerTransformer;
 
@@ -38,8 +43,21 @@ public class MuleLauncher {
                     "test-mule-webservice.xml");
 
             MuleContext context = muleContextFactory.createMuleContext(configBuilder);
+//            LocalMuleClient localMuleClient = context.getClient();
             context.start();
-           
+            Collection<Transformer> tt = context.getRegistry().getTransformers();
+            RegisterInput re = new RegisterInput();
+            RegistrationDetails details = new RegistrationDetails();
+            details.setMonths(BigInteger.valueOf(55L));
+            details.setPaymentPlan("@@@@@@@@@@@@@@@@@@@");
+            re.setName("BONZO");
+            re.setPassword("DOG");
+            re.setRegistrationDate(Calendar.getInstance());
+            re.setRegistrationDetails(details);
+            RegistrationServiceWSDL port = RegistrationServiceWSDLClient.createPort();
+            RegisterResponse reply = port.register(re);
+  //          JAXBUnmarshallerTransformer tr = (JAXBUnmarshallerTransformer) context.getRegistry().get("stringToResponse");
+            log.info("response " + reply.getResponseInformation());
 
             // context.stop();
             // context.dispose();
