f6251d6367edf9b10ec49525cec70972fa9ccd7c
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-04-09 15:17:46 +0000

    working demo of json

---
+++ spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/neo4j/hospital/json/HospitalNode.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.neo4j.hospital.json;
+
+import java.util.List;
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+import org.codehaus.jackson.annotate.JsonTypeInfo.As;
+
+/**
+ *
+ * @author dhenton
+ */
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=As.PROPERTY, property="type")
+  @JsonSubTypes({
+
+        @JsonSubTypes.Type(name="Division",value=Division.class),
+        @JsonSubTypes.Type(name="Provider",value=Provider.class)
+
+    }) 
+public interface HospitalNode {
+
+    String getName();
+    void setName(String t);
+    List<HospitalNode> getChildren();
+    void setChildren(List<HospitalNode> children);
+    Long getId();
+    void setId(Long id);
+    //public String getNodeType();
+    //public void setNodeType(String t);
+   
+     
+}
