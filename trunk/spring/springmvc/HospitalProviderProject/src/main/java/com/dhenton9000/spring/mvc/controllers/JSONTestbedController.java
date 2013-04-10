f1368a03838532dadd4e3facaa0e56ff99d3d036
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-04-10 13:18:18 +0000

    set up json ted bed reinstated mvc:annotation-driven

---
+++ trunk/spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/spring/mvc/controllers/JSONTestbedController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.spring.mvc.controllers;
+
+import com.dhenton9000.neo4j.hospital.json.Division;
+import com.dhenton9000.neo4j.hospital.json.TestBook;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author dhenton
+ */
+
+
+@Controller
+@RequestMapping("json/testbed/*")
+public class JSONTestbedController {
+    
+    
+    private static Logger log = LogManager.getLogger(JSONTestbedController.class);
+    
+    public static final String DESTINATION_TILE = "tiles.json.testbed";
+    public static final String RESULTS_KEY = "results";
+  
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    public ModelAndView showFormInInitialState() {
+
+        return new ModelAndView(DESTINATION_TILE);
+
+    }
+    
+     @RequestMapping(value = "doBook", 
+             produces={"application/json"},
+             consumes={"application/json"} ,
+             method = {RequestMethod.POST,RequestMethod.GET})
+    public @ResponseBody
+    TestBook getShopInJSON(@RequestBody TestBook book) {
+         log.debug("got book: "+book);
+         book.setAuthor(book.getAuthor()+"XXX");
+         return book;
+    }
+    
+}
