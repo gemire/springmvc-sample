3201a3da3ff94d2f697d621986607339c81727d3
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-04-12 20:00:06 +0000

    adding new methods

--- trunk/spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/neo4j/hospital/service/HospitalService.java
+++ trunk/spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/neo4j/hospital/service/HospitalService.java
@@ -7,61 +7,76 @@ package com.dhenton9000.neo4j.hospital.service;
 import com.dhenton9000.neo4j.hospital.json.Division;
 import com.dhenton9000.neo4j.hospital.json.HospitalNode;
 import com.dhenton9000.neo4j.hospital.json.HospitalServiceException;
+import com.dhenton9000.neo4j.hospital.json.Provider;
 import java.io.IOException;
 import java.util.List;
 
-
 /**
- * This service handles divisions and providers. There should never be
- * any direct reference to nodes or indices
+ * This service handles divisions and providers. There should never be any
+ * direct reference to nodes or indices
+ *
  * @author dhenton
  */
 public interface HospitalService {
 
-   
-    
     /**
-     * Build a division object from a point in the tree using the start
-     * points division label. For example, build the entire tree starting
-     * from an existing  node, e.g.  'Midwest'.  
-     * 
+     * Build a division object from a point in the tree using the start points
+     * division label. For example, build the entire tree starting from an
+     * existing node, e.g. 'Midwest'.
+     *
      * @param startDivisionLabel
      * @return an assembled division object
      */
     Division buildDivisonFromDb(String startDivisionLabel);
-    
-    
- 
+
     /**
      * given a proper JSON string, return the division object
+     *
      * @param jsonString
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
     Division stringToStructure(String jsonString) throws IOException;
+
     /**
      * given a proper division object get the JSON string
+     *
      * @param root
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
     String structureToString(Division root) throws IOException;
-    
+
     /**
-     * Given a division attach this whole tree to the root node of the
-     * neo4j graph. The id property can be empty
-     * @param d the graph as as division, this is only a sample, ids will
-     * be filled in when attached
+     * Given a division attach this whole tree to the root node of the neo4j
+     * graph. The id property can be empty
+     *
+     * @param d the graph as as division, this is only a sample, ids will be
+     * filled in when attached
      * @return the root Division with the id now filled in
-     * @throws  when there is a service error, eg trying to attach a division
+     * @throws when there is a service error, eg trying to attach a division
      * that already exists
      */
-    public Division attachFullTree(Division d) throws HospitalServiceException;
-    
+    Division attachFullTree(Division d) throws HospitalServiceException;
+
     /**
-     * 
+     *
      * @param list of nodes
      * @return JSON array
      */
-    public String divArrayToString(List<HospitalNode> list) throws IOException;
+    String divArrayToString(List<HospitalNode> list) throws IOException;
+
+    /**
+     * Attach a provider to a division node. Will handle the following logic
+     * <ul> 
+     * <li>cannot attach a provider to a provider</li> 
+     * <li>parent cannot have any other Divisions as children</li> 
+     * </ul> 
+     * The Division must have an id or a label/name at least one
+     *
+     * @param p
+     * @param parent
+     * @return the Provider with a real id or null if parent not found
+     */
+    Provider attachProvider(Division parent, Provider provider);
 }
--- trunk/spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/neo4j/hospital/service/HospitalServiceImpl.java
+++ trunk/spring/springmvc/HospitalProviderProject/src/main/java/com/dhenton9000/neo4j/hospital/service/HospitalServiceImpl.java
@@ -13,12 +13,6 @@ import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.Transaction;
-import static com.dhenton9000.neo4j.hospital.service.HospitalNeo4jDao.*;
 
 /**
  * Service class for processing JSON for the Provider structures
@@ -27,42 +21,12 @@ import static com.dhenton9000.neo4j.hospital.service.HospitalNeo4jDao.*;
  */
 public class HospitalServiceImpl implements HospitalService  {
 
-    private GraphDatabaseService neo4jDb;
+    
     private ObjectMapper mapper = new ObjectMapper();
-    private static Logger log = LogManager.getLogger(HospitalServiceImpl.class);
+    private final Logger log = LogManager.getLogger(HospitalServiceImpl.class);
     private HospitalNeo4jDao hospitalDao = null;
 
-    @Override
-    public Division attachFullTree(Division d) throws HospitalServiceException {
-        Transaction tx = getHospitalDao().getNeo4jDb().beginTx();
-        Node rootNode = null;
-        String nodeLabel = d.getName();
-        Node testNode = getHospitalDao().getDivisionNode(nodeLabel);
-        if (testNode == null) {
-            try {
-                Node graphRefNode = getHospitalDao().getNeo4jDb().getReferenceNode();
-                log.debug("got in attach " + nodeLabel);
-                rootNode = getHospitalDao().createAndAttachDivisionNode(graphRefNode, nodeLabel);
-                d.setId(rootNode.getId());
-                for (HospitalNode dChild : d.getChildren()) {
-                    getHospitalDao().attachSubTree(dChild, rootNode);
-                }
-
-                tx.success();
-
-            } catch (Exception err) {
-                log.error("error in attachFullTree\n " + err.getClass().getName()
-                        + " " + err.getMessage());
-            } finally {
-                tx.finish();
-            }
-            return buildDivisonFromDb(nodeLabel);
-        } else {
-            throw new HospitalServiceException("node already exists " + nodeLabel);
-        }
-
-    }
-
+  
    
    
 
@@ -83,61 +47,9 @@ public class HospitalServiceImpl implements HospitalService  {
         return mapper.readValue(jsonString, Division.class);
     }
 
-    @Override
-    public Division buildDivisonFromDb(String startDivisionLabel) {
-        Division root = new Division();
-        Node dItem = getHospitalDao().getDivisionNode(startDivisionLabel);
-        if (dItem == null) {
-            throw new RuntimeException("cannot find a node for '" + startDivisionLabel + "'");
-        }
-        String nextItem = (String) dItem.getProperty(DIVISION_DISPLAY_PROPERTY);
-        root.setName(nextItem);
-        root.setId(dItem.getId());
-        buildDivisionElement(dItem, root);
-        return root;
-    }
+   
 
-    private void buildDivisionElement(Node item, HospitalNode parent) {
-        String nextItem = getHospitalDao().getDisplayMessage(item);
-        Iterable<Relationship> rels =
-                item.getRelationships(Direction.OUTGOING);
-        parent.setName(nextItem);
-
-        if (rels.iterator().hasNext()) {
-            for (Relationship r : rels) {
-                HospitalNode hNode = new Division();
-                Node currentNode = r.getEndNode();
-                NODE_TYPE type = getHospitalDao().getNodeType(currentNode);
-                switch (type) {
-                    case DIVISIONS:
-
-                        hNode = new Division();
-                        break;
-
-                    case PROVIDERS:
-                        hNode = new Provider();
-                        break;
-
-                    default:
-                        throw new RuntimeException(" got fault " + currentNode.toString());
-                }
-
-
-                String lVar = getHospitalDao().getDisplayMessage(currentNode);
-                hNode.setName(lVar);
-                hNode.setId(currentNode.getId());
-                parent.getChildren().add(hNode);
-                buildDivisionElement(currentNode, hNode);
-            }
-
-
-        }
-
-
-
-
-    }
-
+   
     /**
      * @return the hospitalDao
      */
@@ -150,6 +62,19 @@ public class HospitalServiceImpl implements HospitalService  {
      */
     public void setHospitalDao(HospitalNeo4jDao hospitalDao) {
         this.hospitalDao = hospitalDao;
+    }
+
+    public Division attachFullTree(Division d) throws HospitalServiceException {
+        return getHospitalDao().attachFullTree(d);
+    }
+
+    public Division buildDivisonFromDb(String startDivisionLabel) {
+         return getHospitalDao().buildDivisonFromDb(startDivisionLabel);
+    }
+
+    public Provider attachProvider(Division parent, Provider p) {
+        
+         return getHospitalDao().attachProvider(parent,p);
     }
 
   
