a25a3e4ae8e4819d0768d4d03c9c282ccd440717
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-04-20 14:50:21 +0000

    added node name to id testing function

--- spring/springmvc/HospitalProviderProject/src/test/java/com/dhenton9000/neo4j/selenium/SimpleTest.java
+++ spring/springmvc/HospitalProviderProject/src/test/java/com/dhenton9000/neo4j/selenium/SimpleTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
-
 /**
  *
  * @author dhenton
@@ -20,31 +19,57 @@ public class SimpleTest extends HospitalNeo4jSeleniumTestBase {
 
     private final Logger logger = LoggerFactory.getLogger(SimpleTest.class);
 
-        
-    
     @Test
     public void testGetName() {
-        
-        
+
+
         String selectedNode = go().selectATree(1).getSelectedNodeName();
         String formText = this.getFormText("maintainName");
-        assertEquals(formText,selectedNode);
-         
+        assertEquals(formText, selectedNode);
+
     }
-    
+
     @Test
-    public void testGetNodeId()
-    {
+    public void testGetNodeId() {
         Long selectedNode = go().selectATree(1).getSelectedNodeId();
-        assertEquals(new Long(3),selectedNode);
+        assertEquals(new Long(1), selectedNode);
     }
-    
+
     @Test
-    public void testAddANodeToCurrent()
-    {
+    public void testGetNodeIdForName() {
+        int t = go().selectATree("snarf").getNodeIdForName("b1");
+        assertEquals(8, t);
+    }
+
+    @Test
+    public void testDelete() {
+        go().createTree("monsoon").addNodeToCurrent("bratwurst").removeNode("bratwurst").removeNode("monsoon");
+
+    }
+
+    @Test
+    public void testFindErrors() {
+        go().createTree("monsoon");
+        String t = getErrorText();
+        logger.debug("t is " + t);
+        assertNotNull(t);
+        // assertNull(getErrorText());
+        // addNodeToCurrent("monsoon");
+        // assertNotNull(getErrorText());
+
+    }
+
+    @Test
+    public void testGetNodeIdForNameFail() {
+        int t = go().selectATree("snarf").getNodeIdForName("gasbag");
+        assertEquals(-1, t);
+    }
+
+    @Ignore
+    public void testAddANodeToCurrent() {
         Long rootId = go().selectATree("alpha").getSelectedNodeId();
-        String selectedNode = selectNode(rootId.intValue()+2).getSelectedNodeName();
+        String selectedNode = selectNode(rootId.intValue() + 2).getSelectedNodeName();
         String formText = this.getFormText("maintainName");
-        assertEquals(formText,selectedNode);
+        assertEquals(formText, selectedNode);
     }
 }
