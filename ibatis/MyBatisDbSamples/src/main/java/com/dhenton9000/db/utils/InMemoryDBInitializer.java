2077c6cab1bc3baf4549e097c813c29872bfae61
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-07-31 03:44:36 +0000

    updates

--- ibatis/MyBatisDbSamples/src/main/java/com/dhenton9000/db/utils/InMemoryDBInitializer.java
+++ ibatis/MyBatisDbSamples/src/main/java/com/dhenton9000/db/utils/InMemoryDBInitializer.java
@@ -19,59 +19,50 @@ import org.slf4j.LoggerFactory;
  */
 public class InMemoryDBInitializer {
 
-     
-	private static final Logger logger = LoggerFactory.getLogger(InMemoryDBInitializer.class);
+    private static final Logger logger = LoggerFactory.getLogger(InMemoryDBInitializer.class);
+    private static boolean didInit = false;
 
-        public InMemoryDBInitializer()
-        {
-            logger.info("called constructor");
-        }
-        
-        public void doInit() throws Exception
-        {
+    public InMemoryDBInitializer() {
+        logger.info("called constructor");
+    }
+
+    public void doInit() throws Exception {
+        if (!didInit) {
             logger.info("called doInit");
+
             String dbURL = "jdbc:derby:memory:demo;create=true";
             Connection conn = null;
+            didInit = true;
             try {
                 logger.info("starting script run");
                 Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
                 conn = DriverManager.getConnection(dbURL);
-                ScriptRunner runner = new ScriptRunner(conn,true,false);
-                
-                runScript(runner,"/db/restaurants_derby.sql");
-                runScript(runner,"/db/sec_derby.sql");
-             } catch (java.sql.SQLException sqle) {
-               
+                ScriptRunner runner = new ScriptRunner(conn, true, false);
+
+                runScript(runner, "/db/restaurants_derby.sql");
+                runScript(runner, "/db/sec_derby.sql");
+            } catch (java.sql.SQLException sqle) {
+
                 throw sqle;
             }
         }
-        
-        
-      
-       public static java.io.OutputStream disableDerbyLogFile(){
-     return new java.io.OutputStream() {
-         public void write(int b) throws IOException {
-             // Ignore all log messages
-         }
-     };
-}
-
-    private void runScript(ScriptRunner runner,String scriptPath) throws Exception {
-        InputStream  is = this.getClass().getResourceAsStream(scriptPath);
-         if (is == null)
-        {
-            throw new RuntimeException("cannot find script at "+scriptPath);
-        }
-        InputStreamReader isReader = new InputStreamReader(is);
-        
-        runner.runScript(isReader);
     }
 
- 
+    public static java.io.OutputStream disableDerbyLogFile() {
+        return new java.io.OutputStream() {
+            public void write(int b) throws IOException {
+                // Ignore all log messages
+            }
+        };
+    }
 
-        
-        
-        
-        
-     
+    private void runScript(ScriptRunner runner, String scriptPath) throws Exception {
+        InputStream is = this.getClass().getResourceAsStream(scriptPath);
+        if (is == null) {
+            throw new RuntimeException("cannot find script at " + scriptPath);
+        }
+        InputStreamReader isReader = new InputStreamReader(is);
+
+        runner.runScript(isReader);
+    }
 }
---
+++ ibatis/MyBatisDbSamples/src/test/java/com/dhenton9000/spring/mybatis/demo/AppTest.java
@@ -0,0 +1,43 @@
+package com.dhenton9000.spring.mybatis.demo;
+
+import com.dhenton9000.db.utils.InMemoryDBInitializer;
+import com.dhenton9000.restaurants.dao.RestaurantsDao;
+import com.dhenton9000.restaurants.model.Restaurant;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:inmemory-derby-spring.xml"})
+@Transactional
+public class AppTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+    private static ClassPathXmlApplicationContext context = null;
+
+    @BeforeClass
+    public static void setUp() {
+     
+        System.setProperty("derby.stream.error.method", "InMemoryDBInitializer.disableDerbyLogFile"); 
+         context = new ClassPathXmlApplicationContext("inmemory-derby-spring.xml");
+      //  context.getBean(InMemoryDBInitializer.class);
+    }
+    @Test
+    public void testRestaurantDao() {
+        RestaurantsDao mapper = (RestaurantsDao) context.getBean("restaurantsMapper");
+        List<Restaurant> restaurants = mapper.getAll();
+        assertEquals(50, restaurants.size());
+
+    }
+}
