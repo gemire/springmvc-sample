36285f14c1482e57a8b408ac6352a55dd9b5b5d8
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-08-01 01:07:18 +0000

    added h2 doesnt seem to be working

--- trunk/ibatis/MyBatisDbSamples/src/main/java/com/dhenton9000/db/utils/InMemoryDBInitializer.java
+++ trunk/ibatis/MyBatisDbSamples/src/main/java/com/dhenton9000/db/utils/InMemoryDBInitializer.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,27 +21,31 @@ public class InMemoryDBInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(InMemoryDBInitializer.class);
     private static boolean didInit = false;
+    private String dbURL = null;
+    private String driverClass = null;
+    private List<String> dbscripts = null;
 
     public InMemoryDBInitializer() {
         logger.info("called constructor");
-        
+
     }
 
     public void doInit() throws Exception {
         if (!didInit) {
             logger.info("called doInit");
-           
-            String dbURL = "jdbc:derby:memory:demo;create=true";
+
             Connection conn = null;
             didInit = true;
             try {
                 logger.info("starting script run");
-                Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
-                conn = DriverManager.getConnection(dbURL);
+                Class.forName(getDriverClass()).newInstance();
+                conn = DriverManager.getConnection(this.getDbURL());
                 ScriptRunner runner = new ScriptRunner(conn, true, false);
+                for (String scr : getDbscripts()) {
+                    runScript(runner, scr);
+                }
 
-                runScript(runner, "/db/restaurants_derby.sql");
-                runScript(runner, "/db/sec_derby.sql");
+
             } catch (java.sql.SQLException sqle) {
 
                 throw sqle;
@@ -65,5 +70,47 @@ public class InMemoryDBInitializer {
         InputStreamReader isReader = new InputStreamReader(is);
 
         runner.runScript(isReader);
+    }
+
+    /**
+     * @return the dbURL
+     */
+    public String getDbURL() {
+        return dbURL;
+    }
+
+    /**
+     * @param dbURL the dbURL to set
+     */
+    public void setDbURL(String dbURL) {
+        this.dbURL = dbURL;
+    }
+
+    /**
+     * @return the driverClass
+     */
+    public String getDriverClass() {
+        return driverClass;
+    }
+
+    /**
+     * @param driverClass the driverClass to set
+     */
+    public void setDriverClass(String driverClass) {
+        this.driverClass = driverClass;
+    }
+
+    /**
+     * @return the dbscripts
+     */
+    public List<String> getDbscripts() {
+        return dbscripts;
+    }
+
+    /**
+     * @param dbscripts the dbscripts to set
+     */
+    public void setDbscripts(List<String> dbscripts) {
+        this.dbscripts = dbscripts;
     }
 }
---
+++ trunk/ibatis/MyBatisDbSamples/src/test/java/com/dhenton9000/spring/mybatis/h2/demo/H2Test.java
@@ -0,0 +1,39 @@
+package com.dhenton9000.spring.mybatis.h2.demo;
+
+import com.dhenton9000.restaurants.dao.RestaurantsDao;
+import com.dhenton9000.restaurants.model.Restaurant;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:inmemory-h2-spring.xml"})
+//@Transactional
+public class H2Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(H2Test.class);
+    private static ClassPathXmlApplicationContext context = null;
+
+    @BeforeClass
+    public static void setUp() {
+     
+        
+         context = new ClassPathXmlApplicationContext("inmemory-h2-spring.xml");
+      //  context.getBean(InMemoryDBInitializer.class);
+    }
+    @Test
+    public void testRestaurantDao() {
+        RestaurantsDao mapper = (RestaurantsDao) context.getBean("restaurantsMapper");
+        List<Restaurant> restaurants = mapper.getAll();
+        assertEquals(50, restaurants.size());
+
+    }
+}
