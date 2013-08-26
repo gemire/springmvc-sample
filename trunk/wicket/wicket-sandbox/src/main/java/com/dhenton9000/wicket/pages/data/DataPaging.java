047d3f8b71f8d61be69278df75348378d53a6650
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-08-26 16:28:48 +0000

    added demo for data view paging

--- trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/data/DataPaging.java
+++ trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/data/DataPaging.java
@@ -4,11 +4,18 @@
  */
 package com.dhenton9000.wicket.pages.data;
 
+import com.dhenton9000.wicket.components.basic.PageSizeSelectorRepeatingView;
 import com.dhenton9000.wicket.dao.service.IRestaurantService;
 import com.dhenton9000.wicket.data.providers.RestaurantDataProvider;
 import com.dhenton9000.wicket.data.providers.RestaurantsDataView;
 import com.dhenton9000.wicket.pages.TemplatePage;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
@@ -19,7 +26,6 @@ public class DataPaging extends TemplatePage {
 
     @SpringBean
     private IRestaurantService service;
-    
 
     public DataPaging() {
         super();
@@ -29,15 +35,40 @@ public class DataPaging extends TemplatePage {
 
     private void setup() {
         RestaurantDataProvider dataView = new RestaurantDataProvider(service);
-
-        RestaurantsDataView rDview = new RestaurantsDataView("repeating", dataView);
+   
+        final RestaurantsDataView rDview = new RestaurantsDataView("repeating", dataView);
+        final PagingNavigator pNav = new PagingNavigator("nav", rDview);
         rDview.setItemsPerPage(10);
-        this.add(rDview);
-        
-        PagingNavigator pNav = new PagingNavigator("nav",rDview);
+        final WebMarkupContainer wContainer = new WebMarkupContainer("tableContainer");
+        wContainer.add(rDview);
+        wContainer.setOutputMarkupId(true);
+        this.add(wContainer);
+        List<Integer> sizeItems = new ArrayList<Integer>();
+        sizeItems.add(5);
+        sizeItems.add(10);
+        sizeItems.add(20);
+        rDview.setOutputMarkupId(true);
+        pNav.setOutputMarkupId(true);
+        PageSizeSelectorRepeatingView pageSelectorView =
+                new PageSizeSelectorRepeatingView("pageSizeSelector", sizeItems) {
+            @Override
+            protected DataView getPagedComponent() {
+                return rDview;
+              }
+
+            @Override
+            protected void addComponents(AjaxRequestTarget target) {
+                 target.add(wContainer);
+                 target.add(pNav);
+            }
+        };
+        {
+        };
+
         this.add(pNav);
+        this.add(pageSelectorView);
         
+
     }
-    
 }//
 
