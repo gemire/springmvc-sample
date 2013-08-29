9e22cc42a47fcbdd24809949d3caec923a86ff7e
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-08-29 14:26:10 +0000

    Mockito inject mocks demo

---
+++ trunk/spring/springdemos/src/main/java/com/dhenton9000/springdemos/annotations/ReportService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.springdemos.annotations;
+
+/**
+ *
+ * @author dhenton
+ */
+public interface ReportService {
+    
+    public String report();
+}
