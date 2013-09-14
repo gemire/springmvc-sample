688f8776432dedf67bc11a3b11fc92e20527fed4
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-09-14 16:53:15 +0000

    no longer needed replaced by GoogleSpringMVCEclipse

--- spring/GoogleSpringMVC/src/main/java/com/dhenton9000/spring/mvc/jdo/dao/RestaurantDao.java
+++
@@ -1,5 +0,0 @@
-package com.dhenton9000.spring.mvc.jdo.dao;
-
-public interface RestaurantDao {
-
-}
--- spring/GoogleSpringMVC/src/main/java/com/dhenton9000/spring/mvc/model/InquiryType.java
+++
@@ -1,5 +0,0 @@
-package com.dhenton9000.spring.mvc.model;
-
-public enum InquiryType {
-	comment, feedback, suggestion;
-}
