81edf817a89ace68bc2c95ad9aedcbfd3ed620fd
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-09-27 20:11:44 +0000

    Panel swapping working, next step is to add form fields

---
+++ wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/subclass/GammaOnePanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.wicket.pages.subclass;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ *
+ * @author dhenton
+ */
+public class GammaOnePanel extends Panel {
+    public GammaOnePanel(String id)
+    {
+        super(id);
+    }
+}
