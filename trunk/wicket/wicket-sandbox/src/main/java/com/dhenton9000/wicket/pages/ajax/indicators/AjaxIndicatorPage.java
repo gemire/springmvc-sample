14e48916fe127f433f4c6b27ed3002834ff036f7
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-10-17 17:45:50 +0000

    simple ajax indicator demo complete

--- trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/ajax/indicators/AjaxIndicatorPage.java
+++ trunk/wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/pages/ajax/indicators/AjaxIndicatorPage.java
@@ -5,20 +5,64 @@
 package com.dhenton9000.wicket.pages.ajax.indicators;
 
 import com.dhenton9000.wicket.pages.TemplatePage;
+import com.dhenton9000.wicket.service.ISlowService;
+import java.util.Date;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
 
 /**
- *
+ * http://stackoverflow.com/questions/11773885/customizing-indicatingajaxlink-in-wicket
  * @author dhenton
  */
 public class AjaxIndicatorPage extends TemplatePage{
+    
+    @SpringBean
+    ISlowService service ;
+    private IModel<String> resultModel = null;
     
      public AjaxIndicatorPage() {
         super();
 
         setPageTitle(getClass().getSimpleName());
         
+        setup();
 
+    }
 
+    private void setup() {
+         final WebMarkupContainer basicIndicatorContainer = new
+                 WebMarkupContainer("basicIndicatorContainer");
+         basicIndicatorContainer.setOutputMarkupId(true);
+         basicIndicatorContainer.setOutputMarkupPlaceholderTag(true);
+         add(basicIndicatorContainer);
+         resultModel = Model.of("Result");
+         
+         Label basicIndicatorResult = new Label("basicIndicatorResult",resultModel);
+         basicIndicatorContainer.add(basicIndicatorResult);
+         IndicatingAjaxLink basicIndicatorButton = 
+                 new IndicatingAjaxLink("basicIndicatorButton")
+         {
+            
+         
+             @Override
+             public void onClick(AjaxRequestTarget target) {
+                 String dd = (new Date()).toString();
+                 String result = service.slowServiceCall(dd);
+                 resultModel.setObject(result);
+                  target.add(basicIndicatorContainer);
+                  
+             }
+             
+         };
+         add(basicIndicatorButton);
+         
+         
     }
     
     
