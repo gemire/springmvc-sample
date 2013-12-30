9006f0cdfe4d371b79d3a12cb4cb09b09386d36c
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2013-12-30 03:01:17 +0000

    added styling for the paging selector

--- wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/components/basic/PageSizeSelector.java
+++ wicket/wicket-sandbox/src/main/java/com/dhenton9000/wicket/components/basic/PageSizeSelector.java
@@ -7,7 +7,6 @@ package com.dhenton9000.wicket.components.basic;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.event.Broadcast;
@@ -15,11 +14,7 @@ import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.markup.html.navigation.paging.IPageable;
-import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.visit.IVisit;
-import org.apache.wicket.util.visit.IVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,18 +27,39 @@ public class PageSizeSelector extends ListView<Integer> {
 
     private final Logger logger = LoggerFactory.getLogger(PageSizeSelector.class);
     public static final String SIZE_SELECT_LINK = "sizeLink";
-     
+    private int selectedPageSize = 5;
 
-    public PageSizeSelector(final String id, final List<Integer> availablePageSizes ) {
+    public PageSizeSelector(final String id, final List<Integer> availablePageSizes) {
         super(id, new Model<LinkedList<Integer>>(new LinkedList<Integer>(availablePageSizes)));
-         
+        
     }
 
     @Override
     protected void populateItem(ListItem<Integer> item) {
-        item.add(new PageSizeLink(SIZE_SELECT_LINK, item.getModelObject()));
+        //logger.info("size with "+item.getModelObject());
+        Integer displayedValue = item.getModelObject();
+        PageSizeLink p = new PageSizeLink(SIZE_SELECT_LINK, displayedValue);
+        if (displayedValue.intValue() == this.getSelectedPageSize()) {
+            p.setHighlighting(Boolean.TRUE);
+        } else {
+            p.setHighlighting(Boolean.FALSE);
+        }
+        item.add(p);
     }
 
+    /**
+     * @return the selectedPageSize
+     */
+    public int getSelectedPageSize() {
+        return selectedPageSize;
+    }
+
+    /**
+     * @param selectedPageSize the selectedPageSize to set
+     */
+    public void setSelectedPageSize(int selectedPageSize) {
+        this.selectedPageSize = selectedPageSize;
+    }
 
     public class PageSizeLink extends AjaxLink<Integer> {
 
@@ -62,37 +78,26 @@ public class PageSizeSelector extends ListView<Integer> {
         @Override
         public void onClick(AjaxRequestTarget target) {
 
-            updateGroupHighlighting();
             SizeSelectionEvent sE = new SizeSelectionEvent();
             sE.ajaxTarget = target;
             sE.sizeValue = getModelObject().intValue();
+            PageSizeSelector.this.setSelectedPageSize(sE.sizeValue);
             send(getPage(), Broadcast.DEPTH, sE);
-           
+
+
         }
 
-        private void updateGroupHighlighting() {
-            getParent().visitChildren(
-                    PageSizeLink.class,
-                    new IVisitor<PageSizeLink, Void>() {
-                        @Override
-                        public void component(final PageSizeLink link, final IVisit<Void> visit) {
-                            link.setHighlighting(link.equals(PageSizeLink.this));
-                            visit.stop();
-                        }
-                    });
-        }
 
-        private void setHighlighting(final Boolean highlighted) {
+        public void setHighlighting(final Boolean highlighted) {
             final String className = highlighted ? SELECTED_CLASS : NOT_CLASS;
 
             add(new AttributeModifier("class", new Model<String>(className)));
         }
     }
 
-    public class SizeSelectionEvent
-    {
+    public class SizeSelectionEvent {
+
         public int sizeValue = 0;
         public AjaxRequestTarget ajaxTarget = null;
     }
-     
 }
