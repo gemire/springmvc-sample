0ec615ff0ab1fdded7b6caa1926da6fd07519d98
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2014-06-02 20:57:24 +0000

    using spring at the root, placing security

--- templates/SpringMVC3-tiles-sec/src/main/java/com/dhenton9000/spring/mvc/controllers/HomePageController.java
+++ templates/SpringMVC3-tiles-sec/src/main/java/com/dhenton9000/spring/mvc/controllers/HomePageController.java
@@ -1,9 +1,12 @@
 package com.dhenton9000.spring.mvc.controllers;
 
+import java.util.Locale;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.apache.log4j.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -25,10 +28,43 @@ public class HomePageController {
 		return new ModelAndView("tiles.homepage", "message", message);
 	}
 	
-	@RequestMapping("/credits")
-	public ModelAndView creditsPage() {
-		
-		return new ModelAndView("tiles.creditspage");
-	}	
+	    /**
+     * To handle the regular request to the application context. e.g.
+     * http://localhost:8080/learn-websockets-1
+     * 
+     * @param model
+     * @param locale
+     * @return
+     */
+    @RequestMapping("/")
+    public ModelAndView handleIndexPage(Model model, Locale locale) {
+        log.info("Request for default / url processed ");
+        return new ModelAndView("tiles.login");
+    }
+
+    /**
+     * Method is executed when there is a call to the <code>/logoutPage</code>
+     * url.
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/logoutPage", method = RequestMethod.GET)
+    public String logoutPage() {
+        log.info("Request for /logoutPage url processed");
+        return "logoutPage";
+    }
+
+    /**
+     * Method is executed when there is a call to the <code>/loginPage</code>
+     * url. On successful login, the user is re-directed to the
+     * <code>/secured/myPage</code> url.
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/loginPage", method = RequestMethod.GET)
+    public String loginPage() {
+        log.info("Request for /loginPage url processed");
+        return "loginPage";
+    }
 	
 }
