56503e7c645b73b1a118555e3da4ba5de45b7f64
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2014-06-04 14:06:00 +0000

    added low level handler demo upgraded jetty

--- trunk/websockets/websocket-demo/src/main/java/com/dhenton9000/spring/mvc/websocket/handlers/EchoHandler.java
+++ trunk/websockets/websocket-demo/src/main/java/com/dhenton9000/spring/mvc/websocket/handlers/EchoHandler.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.dhenton9000.spring.mvc.websocket.handlers;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -16,15 +17,20 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
  */
 public class EchoHandler extends TextWebSocketHandler {
 
+    private static Logger LOG = LogManager.getLogger(EchoHandler.class);
+
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) 
+    protected void handleTextMessage(WebSocketSession session, TextMessage message)
             throws Exception {
-          
+        String mm = message.getPayload();
+        LOG.info("got '"+mm+"' in handler");
+        TextMessage reply = new TextMessage("Bonzo " + mm);
+        session.sendMessage(reply);
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-         
+        LOG.debug("connection established " + session.getPrincipal());
     }
-    
+
 }
