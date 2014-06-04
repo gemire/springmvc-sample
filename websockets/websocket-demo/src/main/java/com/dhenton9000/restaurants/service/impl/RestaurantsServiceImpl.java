a6ac3588eb25a557e5c9497c4d16582aa292c12a
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2014-06-04 17:54:47 +0000

    added in memory derby database

---
+++ websockets/websocket-demo/src/main/java/com/dhenton9000/restaurants/service/impl/RestaurantsServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dhenton9000.restaurants.service.impl;
+import com.dhenton9000.restaurants.service.RestaurantService;
+/**
+ *
+ * @author dhenton
+ */
+public class RestaurantsServiceImpl implements RestaurantService {
+    
+}
