a87a5a8c5d22f3d1ba5108687fd89c041287b6a7
donhenton@gmail.com <donhenton@gmail.com@d940f8c5-0c11-74e3-e893-397f882b9707>
2014-06-17 13:09:47 +0000

    demo for use of futures and threading

---
+++ trunk/threads/FutureDemo/src/main/java/com/dhenton9000/future/demo/services/IService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dhenton9000.future.demo.services;
+
+/**
+ *
+ * @author dhenton
+ */
+public interface IService {
+    
+    String getMessage(String input);
+}
